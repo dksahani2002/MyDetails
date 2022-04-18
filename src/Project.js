d9b60351bb48f33b54e1a6e40f087522b4e11e95
Durgeshwar Sahani <rd113760@gmail.com>
2022-04-18 18:55:09 +0530

    added project

--- src/Project.js
+++ src/Project.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./Project.css";
 import Project1 from './img/crypto.jpg';
- 
+import Project3 from './img/studentdata.jpg';
 import Project2 from './img/realestate.jpeg';
-
+import Project4 from './img/weather.jpg';
 function Project() {
   return (
     <div className="project component__space" id="Portfolio">
@@ -19,6 +19,57 @@ function Project() {
        <div className="container">
            <div className="row">
              
+             
+ 
+
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project3} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Development</h5>
+                     <h4 className="project__text">Student Local Database And Basic Information</h4>
+                     <a href="https://dddweb.herokuapp.com/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project4} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Development</h5>
+                     <h4 className="project__text">Buy Sell or Rent House</h4>
+                     <a href="https://realestatewithdd-q6uehd78n-dksahani2002.vercel.app/" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div> 
+             <div className="col__3">
+                 <div className="project__box pointer relative">
+                     <div className="project__box__img pointer relative">
+                         <div className="project__img__box">
+                             <img src={Project2} alt="" className="project__img" />
+                         </div>
+                         <div className="mask__effect"></div>
+                     </div>
+                     <div className="project__meta absolute">
+                     <h5 className="project__text">Development</h5>
+                     <h4 className="project__text">WeatherApp</h4>
+                     <a href="https://github.com/dksahani2002/WeatherApp" className="project__btn">View Details</a>
+                     </div>
+                 </div>
+             </div>
+
+             
+              
              <div className="col__3">
                  <div className="project__box pointer relative">
                      <div className="project__box__img pointer relative">
@@ -30,29 +81,10 @@ function Project() {
                      <div className="project__meta absolute">
                      <h5 className="project__text">Development</h5>
                      <h4 className="project__text">Explore The Crypto with CryptoDd</h4>
-                     <a href="https://cryptodetailsd.netlify.app/" className="project__btn">View Details</a>
+                     <a href="https://cryptodetailsd.netlify.app/" className="project__btn">API Down (No Details)</a>
                      </div>
                  </div>
              </div>
- 
-
-             <div className="col__3">
-                 <div className="project__box pointer relative">
-                     <div className="project__box__img pointer relative">
-                         <div className="project__img__box">
-                             <img src={Project2} alt="" className="project__img" />
-                         </div>
-                         <div className="mask__effect"></div>
-                     </div>
-                     <div className="project__meta absolute">
-                     <h5 className="project__text">Development</h5>
-                     <h4 className="project__text">Buy Sell or Rent House</h4>
-                     <a href="https://realestatewithdd-q6uehd78n-dksahani2002.vercel.app/" className="project__btn">View Details</a>
-                     </div>
-                 </div>
-             </div> 
-              
-              
              {/* <div className="view__more__btn d__flex align__items__center justify__content__center pxy__30">
              <button className="view__more pointer btn">View more</button>
              </div> */}
